944e2026b41d66a37a26a4f937274523bb38f0d0
Hilton M Moreira <hiltonmm@gmail.com>
2021-10-11 10:49:25 -0300

    Define o Layout da tela inicial

---
+++ cards.php
@@ -0,0 +1,103 @@
+<div class="container p-3">
+<div class="row">
+    <div class="col">
+
+        <div class="card text-dark bg-light mb-3" style="max-width: 25rem;">
+            <div class="card-header">Aviso nº 123/2021</div>
+            <div class="card-body">
+                <h5 class="card-title">Registro</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="col">
+
+        <div class="card text-dark bg-light mb-3" style="max-width: 25rem;">
+            <div class="card-header">Header</div>
+            <div class="card-body">
+                <h5 class="card-title">Todos</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="col">
+
+<div class="card text-dark bg-light mb-3" style="max-width: 25rem;">
+    <div class="card-header">Header</div>
+    <div class="card-body">
+        <h5 class="card-title">Light card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+</div>
+
+</div>
+
+
+
+
+</div>
+<div class="row">
+    <div class="col">
+
+        <div class="card text-dark bg-light mb-3" style="max-width: 25rem;">
+            <div class="card-header">Header</div>
+            <div class="card-body">
+                <h5 class="card-title">Light card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="col">
+
+        <div class="card text-dark bg-light mb-3" style="max-width: 25rem;">
+            <div class="card-header">Header</div>
+            <div class="card-body">
+                <h5 class="card-title">Light card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="col">
+
+<div class="card text-dark bg-light mb-3" style="max-width: 25rem;">
+    <div class="card-header">Header</div>
+    <div class="card-body">
+        <h5 class="card-title">Light card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+</div>
+
+</div>
+
+
+
+
+</div>
+
+
+
+<nav>
+  <ul class="pagination justify-content-center">
+    <li class="page-item disabled">
+      <a class="page-link">Anterior</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">Proxima</a>
+    </li>
+  </ul>
+</nav>
+
+
+
+</div>
--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
 <html lang="pt-br">
     <head>
         <meta charset="utf-8">
@@ -12,11 +8,11 @@
         <link rel="stylesheet" href="/css/bootstrap.min.css">
         <script src="/js/jquery-3.6.0.min.js"></script>
     </head>
-    <body>
-        <!--[if lt IE 7]>
-           <p class="browsehappy">Você está usando um navegador <strong>desatualizado</strong>. Atualize seu navegador para melhorar sua experiência.</p>
-        <![endif]-->
+    <body> 
+        <?php require_once("navBar.php");
+        require_once("cards.php");
         
+        ?>   
         <script src="/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
