25fe3c1c4c948ebbe745644ad5091319061325bd
Hilton M Moreira <hiltonmm@gmail.com>
2021-10-11 09:39:05 -0300

    Jquery Install

--- index.php
+++ index.php
@@ -9,13 +9,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Central de Aviso</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <script src="/js/jquery-3.6.0.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 7]>
            <p class="browsehappy">Você está usando um navegador <strong>desatualizado</strong>. Atualize seu navegador para melhorar sua experiência.</p>
         <![endif]-->
         
-        <script src="" async defer></script>
+        <script src="/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
